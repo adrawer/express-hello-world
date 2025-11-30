08b7fa9d4e3452da6b2ac0f1ec248a2c87d0ae9d
Andrew <andrew.drawer@gmail.com>
2025-11-30 01:07:51 -0600

    Update and rename app.json to index.js

---
+++ index.js
@@ -0,0 +1,97 @@
+var Botkit = require('botkit');
+
+var PORT = process.env.PORT
+if (!PORT) {
+    console.error('PORT is required');
+    process.exit(1);
+}
+
+var VERIFY_TOKEN = process.env.SLACK_TOKEN
+if (!VERIFY_TOKEN) {
+  console.error('SLACK_TOKEN is required')
+  process.exit(1)
+}
+
+var controller = Botkit.slackbot({
+  // reconnect to Slack RTM when connection goes bad
+  retry: Infinity,
+  debug: false
+});
+
+console.log('Starting in Beep Boop multi-team mode')
+require('beepboop-botkit').start(controller, { debug: true })
+
+controller.setupWebserver(PORT, function(err, webserver) {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    // Setup our slash command webhook endpoints
+    controller.createWebhookEndpoints(webserver)
+});
+
+controller.on('slash_command', function(bot, message) {
+    if (message.token !== VERIFY_TOKEN) {
+        return bot.res.send(401, 'Unauthorized');
+    }
+    if (message.command !== "/spoiler") {
+        bot.replyAcknowledge();
+        return;
+    }
+    
+    var user = message.user_name;
+    var params = message.text.match(/\w+|"[^"]+"/g); //split our (possibly quoted) params
+    if (!params || params.length === 0) {
+        bot.replyPrivate(message, {
+          response_type: 'ephemeral',
+          text: 'You didnt enter anything to spoiler text.'
+        });
+        return;
+    }
+    
+    var title, spoiler;
+    if (message.text.indexOf('"') < 0) {
+        title = '@' + user + ' just posted a spoiler.';
+        spoiler = message.text;
+    } else if (params.length === 1) {
+        title = '@' + user + ' just posted a spoiler.';
+        spoiler = params[0];
+    } else {
+        title = '@' + user + ' just posted a spoiler ' + params[0];
+        params.splice(0, 1);
+        spoiler = params.join(' ');
+    }
+
+    bot.replyPublicDelayed(message, {
+        "response_type": "in_channel",
+        "attachments": [{
+            "callback_id": "spoiler-callback",
+            "title": title,
+            "attachment_type": "default",
+            "fallback": "Whoops - looks like your device isnt supported",
+            "actions": [{
+                "name": "view",       
+                "text": "Click to view spoiler",
+                "style": "danger",
+                "type": "button",
+                "confirm": {
+                    "title": title,
+                    "text": spoiler,
+                    "ok_text": "Close",
+                    "dismiss_text": "Close"
+                }
+            }]
+        }]
+    }, function() {
+        return bot.res.send(200, '');
+    });
+});
+
+// receive an interactive message, and reply with a message that will replace the original
+controller.on('interactive_message_callback', function(bot, message) {
+    if (message.token !== VERIFY_TOKEN) {
+        return bot.res.send(401, 'Unauthorized');
+    }
+    
+    return bot.res.send(200, ''); //do nothing
+});
